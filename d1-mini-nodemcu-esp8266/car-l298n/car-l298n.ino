f3a322099d7d287ace82c8ebb36604a0f216cbea
Hung P Le <hungple@us.ibm.com>
2019-12-22 21:52:10 -0800

    Fix watchdog

--- d1-mini-nodemcu-esp8266/car-l298n/car-l298n.ino
+++ d1-mini-nodemcu-esp8266/car-l298n/car-l298n.ino
@@ -28,25 +28,26 @@ Ticker secondTick;
 volatile int watchdogCount = 0;
 
 // Current time
-unsigned long currentTime = millis();
+//unsigned long currentTime = millis();
 // Previous time
-unsigned long previousTime = 0;
+//unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
+//const long timeoutTime = 2000;
 
 const int MIN_SPEED  = 400;
 const int MAX_SPEED  = 1500;
 const int STEP_SPEED = 100;
 const int TURN_SPEED = 1000;
+const int BACK_SPEED = 600;
+const int WATCHDOG_COUNT_MAX = 5;
+
 int curSpeed = MIN_SPEED;
 
 
-// int back_led = 13;
-// int front_led = 12;
-
 void ISRwatchdog() {
   watchdogCount++;
-  if(watchdogCount == 5) {
+  Serial.println(watchdogCount);
+  if(watchdogCount >= WATCHDOG_COUNT_MAX) {
     Serial.println();
     Serial.println("the watchdog bites!!!!!!!!!!!");
     ESP.reset();
@@ -55,7 +56,7 @@ void ISRwatchdog() {
 
 void setup() {
   Serial.begin(115200);
-  secondTick.attach(1, ISRwatchdog);
+  secondTick.attach(1, ISRwatchdog); // 1st argument in seconds after which the ISRwathdog() executes
 
   // Initialize the output variables as outputs
   pinMode(IN3, OUTPUT);
@@ -95,17 +96,19 @@ void setup() {
 }
 
 void loop() {
-
   WiFiClient client = server.available();   // Listen for incoming clients
   watchdogCount = 0;
+  
+  
   if (client) {                             // If a new client connects,
 
     Serial.println("New Client.");          // print a message out in the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();
+    //currentTime = millis();
+    //previousTime = currentTime;
+    //while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+    while (client.connected()) { // loop while the client's connected
+//      currentTime = millis();
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
@@ -140,7 +143,7 @@ void loop() {
               }
             } else if (header.indexOf("GET /B") >= 0) { // Reverse
               Serial.println("B");
-              MotorB_Run(-600);
+              MotorB_Run(-BACK_SPEED);
             } else if (header.indexOf("GET /L") >= 0) { // Left
               Serial.println("L");
               MotorF_Run(TURN_SPEED);
@@ -153,132 +156,58 @@ void loop() {
             } else {
 
 
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 80px;");
-            client.println("text-decoration: none; font-size: 12px; margin: 1px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}");
-            client.println(".box {width: 150px; height: 150px; padding: 10px; border: 2px solid orange; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; background: lightyellow;}");
-            client.println("</style>");
-            client.println("</head>");
+              // Display the HTML web page
+              client.println("<!DOCTYPE html><html>");
+              client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+              client.println("<link rel=\"icon\" href=\"data:,\">");
 
-            // Web Page Heading
-            client.println("<body>");
-
-//            client.println("<div class='box' id='box1'>");
-//            client.println("<h3> Touch Me! </h3>");
-//            client.println("</div>");
-//            client.println("<h3 id='statusdiv'>Status</h3>");
-//
-//            client.println("<script>");
-//            client.println("window.addEventListener('load', function(){");
-//            client.println("var box1 = document.getElementById('box1')");
-//            client.println("var statusdiv = document.getElementById('statusdiv')");
-//            client.println("var startx = 0");
-//            client.println("var dist = 0");
-//            client.println("box1.addEventListener('touchstart', function(e){");
-//
-//            client.println("  var xhttp = new XMLHttpRequest();");
-//            client.println("  xhttp.open('GET', 'L', true);");
-//            client.println("  xhttp.send();");
-//            client.println("var touchobj = e.changedTouches[0]");
-//            client.println("startx = parseInt(touchobj.clientX)");
-//            client.println("statusdiv.innerHTML = 'Status: touchstart<br> ClientX: ' + startx + 'px'");
-//            client.println("e.preventDefault()");
-//            client.println("}, false)");
-//
-//
-//            client.println("box1.addEventListener('touchmove', function(e){");
-//            client.println("var touchobj = e.changedTouches[0]");
-//            client.println("var dist = parseInt(touchobj.clientX) - startx");
-//            client.println("statusdiv.innerHTML = 'Status: touchmove<br> Horizontal distance traveled: ' + dist + 'px'");
-//            client.println("}, false)");
-//
-//            client.println("box1.addEventListener('touchend', function(e){");
-//            client.println("var touchobj = e.changedTouches[0]");
-//            client.println("statusdiv.innerHTML = 'Status: touchend<br> Resting x coordinate: ' + touchobj.clientX + 'px'");
-//            client.println("  var xhttp = new XMLHttpRequest();");
-//            client.println("  xhttp.open('GET', 'M', true);");
-//            client.println("  xhttp.send();");
-//            client.println("e.preventDefault()");
-//            client.println("}, false)");
-//            client.println("}, false)");
-//            client.println("</script>");
+              // Style sheet: Feel free to change the background-color and font-size attributes to fit your preferences
+              client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+              client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 80px; text-decoration: none; font-size: 12px; margin: 1px; cursor: pointer;}");
+              client.println(".button2 { background-color: #195B6A; border: none; color: white; padding: 60px 150px; text-decoration: none; font-size: 12px; margin: 1px; cursor: pointer;}");
+              client.println(".box {width: 150px; height: 150px; padding: 10px; border: 2px solid orange; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; background: lightyellow;}");
+              client.println("</style>");
+              client.println("</head>");
+  
+              client.println("<body>");
 
 
-//            client.println("<h2>ESP8266 Controller using Javascript</h2>");
-//            client.println("<button class=\"button\" type=\"button\" onclick=\"run('F')\">F</button>");
-//            client.println("<button class=\"button\" type=\"button\" onclick=\"run('S')\">S</button>");
-//            client.println("<button class=\"button\" type=\"button\" onclick=\"run('B')\">B</button>");
-//            client.println("<br>");
-//            client.println("<br>");
-//            client.println("<br>");
-//            client.println("<br>");
-//            client.println("<button class=\"button\" type=\"button\" onclick=\"turn('L')\">L</button>");
-//            client.println("<button class=\"button\" type=\"button\" onclick=\"turn('M')\">M</button>");
-//            client.println("<button class=\"button\" type=\"button\" onclick=\"turn('R')\">R</button>");
-            client.println("<button class=\"button\" type=\"button\" ontouchstart=\"turn('L')\" ontouchend=\"turn('M')\">L</button>");
+              client.println("<h2>ESP8266 Controller</h2>");
+              client.println("<button class=\"button\" type=\"button\" onclick=\"run('F')\">F</button>");
+              client.println("<button class=\"button\" type=\"button\" onclick=\"run('S')\">S</button>");
+              client.println("<button class=\"button\" type=\"button\" onclick=\"run('B')\">B</button>");
+              client.println("<br>");
+              client.println("<br>");
+              client.println("<button class=\"button2\" type=\"button\" ontouchstart=\"turn('L')\" ontouchend=\"turn('M')\"></button>");
+              client.println("<button class=\"button2\" type=\"button\" ontouchstart=\"turn('R')\" ontouchend=\"turn('M')\">R</button>");
+  
+              client.println("<script>");
+              client.println("function run(direction) {");
+              client.println("  var xhttp = new XMLHttpRequest();");
+              client.println("  xhttp.open('GET', direction, true);");
+              client.println("  xhttp.send();");
+              client.println("  return false;");
+              client.println("}");
 
-//
-            client.println("<script>");
-//            client.println("function run(name) {");
-//            client.println("  var xhttp = new XMLHttpRequest();");
-//            client.println("  xhttp.open('GET', name, true);");
-//            client.println("  xhttp.send();");
-//            client.println("  return false;");
-//            client.println("}");
-//
-//            client.println("function turn(name) {");
-//            client.println("  var xhttp = new XMLHttpRequest();");
-//            client.println("  xhttp.open('GET', name, true);");
-//            client.println("  xhttp.send();");
-//            client.println("  sleepFor(200);");
-//            client.println("  var xhttp2 = new XMLHttpRequest();");
-//            client.println("  xhttp2.open('GET', 'M', true);");
-//            client.println("  xhttp2.send();");
-//            client.println("  return false;");
-//            client.println("}");
-//
-//            client.println("function sleepFor( sleepDuration ){");
-//            client.println("  var now = new Date().getTime();");
-//            client.println("  while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } ");
-//            client.println("}");
-//
-
-            client.println("function turn(name) {");
-            client.println("  var xhttp = new XMLHttpRequest();");
-            client.println("  xhttp.open('GET', name, true);");
-            client.println("  xhttp.send();");
-            client.println("  return false;");
-            client.println("}");
-
-            client.println("</script>");
+              client.println("function turn(direction) {");
+              client.println("  var xhttp = new XMLHttpRequest();");
+              client.println("  xhttp.open('GET', direction, true);");
+              client.println("  xhttp.send();");
+              client.println("  return false;");
+              client.println("}");
+  
+              client.println("function sleepFor( sleepDuration ){");
+              client.println("  var now = new Date().getTime();");
+              client.println("  while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } ");
+              client.println("}");
 
 
+              client.println("</script>");
 
-            // client.println("<h2>ESP8266 Controller using HTML</h2>");
-            // client.println("<center>");
-            // client.println("<a href=\"/F\"><button class=\"button\">F</button></a>");
-            // client.println("<a href=\"/S\"><button class=\"button\">S</button></a>");
-            // client.println("<a href=\"/B\"><button class=\"button\">B</button></a>");
-            // client.println("<br");
-            // client.println("<br");
-            // client.println("<br");
-            // client.println("<br");
-            // client.println("<a href=\"/L\"><button class=\"button\">L</button></a>");
-            // client.println("<a href=\"/M\"><button class=\"button\">M</button></a>");
-            // client.println("<a href=\"/R\"><button class=\"button\">R</button></a>");
-            // client.println("</center>");
+              client.println("</body></html>");
 
-            client.println("</body></html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
+              // The HTTP response ends with another blank line
+              client.println();
             }
             // Break out of the while loop
             break;
@@ -298,11 +227,6 @@ void loop() {
     Serial.println("");
   }
 
-}
-
-void ChangeSpeed(int spd)
-{
- curSpeed = 100*spd;
 }
 
 
