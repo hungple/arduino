8aba40a189e006ac12d0fc19ab0d3612a9701b2d
Simon H Le <simonle0525@gmail.com>
2019-12-27 11:06:34 -0800

    Final version of car

--- d1-mini-nodemcu-esp8266/car-l298n/car-l298n.ino
+++ d1-mini-nodemcu-esp8266/car-l298n/car-l298n.ino
@@ -8,7 +8,7 @@
 #define IN2   12          // L298N IN4 Back motors                GPIO12(D6)
 
 
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include <Ticker.h>
 
 // Load Wi-Fi library
@@ -18,42 +18,42 @@ const char* ssid     = "orange";
 const char* password = "87511222333";
 
 // Set web server port number to 80
-WiFiServer server(80);
+WiFiServer server(80);      // Creates a server that listens for incoming connections on the specified port
 
 // Variable to store the HTTP request
 String header;
 
 // Watchdog
 Ticker secondTick;
-volatile int watchdogCount = 0;
+volatile int watchdogCount = 0;     // A variable which its value can be changed by code outside the 
+                                    // scope of current code at any time. In this case, watchdogCount
+                                    // is changed by ISRwatchdog and the loop functions.
 
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0;
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
-
-const int MIN_SPEED  = 600;
+const int MIN_SPEED  = 400;
+const int MAX_SPEED  = 1200;
 const int STEP_SPEED = 50;
-int curSpeed = 600;
+const int TURN_SPEED = 1000;
+const int BACK_SPEED = 701;
+const int STOP_MULTIPLY = 2;
+const int WATCHDOG_COUNT_MAX = 5;
+
+int curSpeed = MIN_SPEED;
 
 
-// int back_led = 13;
-// int front_led = 12;
-
-void ISRwatchdog() {
+void ISRwatchdog() {      //ISR = Interrupt Service Routine
   watchdogCount++;
-  if(watchdogCount == 5) {
+  Serial.println(watchdogCount);
+  if(watchdogCount >= WATCHDOG_COUNT_MAX) {  // If the loop has not been invoked for a while
     Serial.println();
     Serial.println("the watchdog bites!!!!!!!!!!!");
-    ESP.reset();
+    ESP.reset();       // Restart the esp board
   }
 }
 
 void setup() {
-  Serial.begin(115200);
-  secondTick.attach(1, ISRwatchdog);
+  Serial.begin(115200);       // To connect to the serial moniter
+  secondTick.attach(1, ISRwatchdog);  // Register the watchdog: 1st argument in seconds after which the ISRwatchdog() executes
+                                      // 1 is the time interval in second units between the invocations
   // Initialize the output variables as outputs
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -70,39 +70,32 @@ void setup() {
   digitalWrite(IN4, LOW);
   analogWrite (ENB, 0);
 
-  // back and front leds
-  // pinMode(back_led, OUTPUT);
-  // pinMode(front_led, OUTPUT);
-
   // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
+  Serial.println();
+  Serial.println("Connecting to ");
   Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  WiFi.begin(ssid, password);            // Initializes the WiFi library's network settings
+  while (WiFi.status() != WL_CONNECTED) {      // Checks the current status
+    delay(500);         //delay you by half a second
     Serial.print(".");
   }
   // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
+  Serial.println(WiFi.localIP());      // Prints out the WiFi shield's IP address
+  server.begin();       // Tells the server to begin listening for incoming connections
 
 }
 
 void loop() {
-
-  WiFiClient client = server.available(); // Listen for incoming clients
   watchdogCount = 0;
+  WiFiClient client = server.available(); // Get client to listen for incoming clients
 
-  if (client) {                             // If a new client connects,
+  if (client!=0) {                             // If a new client connects,
     Serial.println("New Client.");          // print a message out in the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
-    currentTime = millis();
-    previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-      currentTime = millis();
+    while (client.connected()) { // loop while the client's connected
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
@@ -111,123 +104,91 @@ void loop() {
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
 
             // Process header/command
             if (header.indexOf("GET /F") >= 0) { // Forward
               Serial.println("F");
-              curSpeed = curSpeed + STEP_SPEED;
+              if (curSpeed < MAX_SPEED) {
+                curSpeed = curSpeed + STEP_SPEED;
+              }
               Serial.println(curSpeed);
               MotorB_Run(curSpeed);
             } else if (header.indexOf("GET /S") >= 0) {
               Serial.println("S");
-              curSpeed = MIN_SPEED;
-              MotorB_Run(0);
+              if (curSpeed > MIN_SPEED) {       // Increase speed
+                curSpeed = curSpeed - STOP_MULTIPLY*STEP_SPEED;
+                Serial.println(curSpeed);
+                MotorB_Run(curSpeed);
+              } else {
+                MotorB_Run(0);
+              }
             } else if (header.indexOf("GET /B") >= 0) { // Reverse
               Serial.println("B");
-              MotorB_Run(-curSpeed);
+              MotorB_Run(-BACK_SPEED);
             } else if (header.indexOf("GET /L") >= 0) { // Left
               Serial.println("L");
-              MotorF_Run(1800);
-            } else if (header.indexOf("GET /M") >= 0) {
+              MotorF_Run(TURN_SPEED);
+            } else if (header.indexOf("GET /M") >= 0) { // Stop
               Serial.println("M");
               MotorF_Run(0);
-            } else if (header.indexOf("GET /R") >= 0) {
+            } else if (header.indexOf("GET /R") >= 0) { // right
               Serial.println("R");
-              MotorF_Run(-1800);
+              MotorF_Run(-TURN_SPEED);
             } else {
+              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+              // and a content-type so the client knows what's coming, then a blank line:
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
+               
+              // Display the HTML web page
+              client.println("<!DOCTYPE html><html>");
+              client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+              client.println("<link rel=\"icon\" href=\"data:,\">");
+              // CSS to style the on/off buttons
+              // Feel free to change the background-color and font-size attributes to fit your preferences
+              client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+              client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px; text-decoration: none; font-size: 12px; margin: 1px; cursor: pointer;}");
+              client.println(".button3 { background-color: #195B6A; border: none; color: white; padding: 48px 70px; text-decoration: none; font-size: 12px; margin: 1px; cursor: pointer;}");
+              client.println("</style>");
+              client.println("</head>");
+
+              // Web Page Heading
+              client.println("<body>");
 
 
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 12px; margin: 1px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}");
-            client.println("</style>");
-            client.println("</head>");
-
-            // Web Page Heading
-            client.println("<body>");
-
-
-
-            client.println("<h2>ESP8266 Controller using JavaScript</h2>");
-            client.println("<button class=\"button\" type=\"button\" onclick=\"run('F')\">F</button>");
-            client.println("<button class=\"button\" type=\"button\" onclick=\"run('S')\">S</button>");
-            client.println("<button class=\"button\" type=\"button\" onclick=\"run('B')\">B</button>");
-            client.println("<br>");
-            client.println("<br>");
-            client.println("<br>");
-            client.println("<br>");
-            client.println("<button class=\"button\" type=\"button\" onclick=\"turn('L')\">L</button>");
-            client.println("<button class=\"button\" type=\"button\" onclick=\"turn('M')\">M</button>");
-            client.println("<button class=\"button\" type=\"button\" onclick=\"turn('R')\">R</button>");
-            client.println("<p id='demo'></p>");
-
-            client.println("<script>");
-            client.println("function run(name) {");
-            client.println("  var xhttp = new XMLHttpRequest();");
-            client.println("  xhttp.open('GET', name, true);");
-            client.println("  xhttp.send();");
-            client.println("  return false;");
-            client.println("}");
-
-            client.println("function turn(name) {");
-            client.println("  var xhttp = new XMLHttpRequest();");
-            client.println("  xhttp.open('GET', name, true);");
-            client.println("  xhttp.send();");
-            client.println("  sleepFor(200);");
-            client.println("  var xhttp2 = new XMLHttpRequest();");
-            client.println("  xhttp2.open('GET', 'M', true);");
-            client.println("  xhttp2.send();");
-
-            client.println("  return false;");
-            client.println("}");
-
-            client.println("function sleepFor( sleepDuration ){");
-            client.println("  var now = new Date().getTime();");
-            client.println("  while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } ");
-            client.println("}");
-            
-            client.println("</script>");
-
-
-
-
-
-
-
-
-            
-//            client.println("<body><h2>ESP8266 Controller using html</h2>");
-//            client.println("<center>");
-//            client.println("<a href=\"/F\"><button class=\"button\">F</button></a>");
-//            client.println("<a href=\"/S\"><button class=\"button\">S</button></a>");
-//            client.println("<a href=\"/B\"><button class=\"button\">B</button></a>");
-//            client.println("<br>");
-//            client.println("<br>");
-//            client.println("<br>");
-//            client.println("<br>");
-//            client.println("<br>");
-//            client.println("<br>");
-//            client.println("<a href=\"/L\"><button class=\"button\">L</button></a>");
-//            client.println("<a href=\"/M\"><button class=\"button\">M</button></a>");
-//            client.println("<a href=\"/R\"><button class=\"button\">R</button></a>");
-//            client.println("</center>");
-//            client.println("</body></html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
+              client.println("<h2>ESP8266 Controller using JavaScript</h2>");
+              client.println("<button class=\"button\" type=\"button\" onclick=\"run('F')\">F</button>");
+              client.println("<button class=\"button\" type=\"button\" onclick=\"run('S')\">S</button>");
+              client.println("<button class=\"button\" type=\"button\" onclick=\"run('B')\">B</button>");
+              client.println("<br>");
+              client.println("<br>");
+              client.println("<button class=\"button3\" type=\"button\" ontouchstart=\"turn('L')\" ontouchend=\"turn('M')\"></button>");
+              client.println("<button class=\"button3\" type=\"button\" ontouchstart=\"turn('R')\" ontouchend=\"turn('M')\"></button>");
+              
+              client.println("<script>");
+              client.println("function run(direction) {");
+              client.println("  var xhttp = new XMLHttpRequest();");
+              client.println("  xhttp.open('GET', direction, true);");
+              client.println("  xhttp.send();");
+              client.println("  return false;");
+              client.println("}");
+              client.println("function turn(direction) {");
+              client.println("  var xhttp = new XMLHttpRequest();");
+              client.println("  xhttp.open('GET', direction, true);");
+              client.println("  xhttp.send();");
+              client.println("  return false;");
+              client.println("}");
+              client.println("function sleepFor( sleepDuration ){");
+              client.println("  var now = new Date().getTime();");
+              client.println("  while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } ");
+              client.println("}");
+              client.println("</script>");
+              
+              client.println("</body></html>");
+              // The HTTP response ends with another blank line
+              client.println();
             }
             // Break out of the while loop
             break;
@@ -249,10 +210,10 @@ void loop() {
 
 }
 
-void ChangeSpeed(int spd)
-{
- curSpeed = 100*spd;
-}
+//void ChangeSpeed(int spd)
+//{
+// curSpeed = 100*spd;
+//}
 
 
 void MotorF_Run(int spd){
